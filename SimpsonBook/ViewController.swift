4998ed061b228b8b073f0998b6f37f024be71dbb
sfctl <catalbasifa@gmail.com>
2021-01-15 08:01:44 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Yasemin YEL on 15.01.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
